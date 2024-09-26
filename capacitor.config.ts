b8c21b7acfcfee623a554dd218940f2987769d01
John Rooney <rooneyjohn@gmail.com>
2024-09-26 19:00:51 -0400

    adjusted paintShop name out of things

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'paintShop',
+  appName: 'calapp2-9420e',
   webDir: 'dist'
 };
 
